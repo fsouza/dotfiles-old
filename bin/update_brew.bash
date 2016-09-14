fed7fa4312f258422e65ea0da3fade2c48ae4eb2
Francisco Souza <fsouza@users.noreply.github.com>
2016-09-14 16:25:09 -0400

    bin/update_brew: add universal-ctags reinstall
    Since I'm using master for this one as well, it's better to update
    everyday. Until they tag it.

--- bin/update_brew.bash
+++ bin/update_brew.bash
@@ -4,6 +4,7 @@ brew update
 brew upgrade
 
 brew reinstall --HEAD --with-release neovim/neovim/neovim
+brew reinstall --HEAD universal-ctags/universal-ctags/universal-ctags
 
 brew cask update
 brew cleanup -s
