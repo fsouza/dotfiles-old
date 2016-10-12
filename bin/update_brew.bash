d6a01aa9e7e6be40665f7246067133c3c7a6ba8e
Francisco Souza <fsouza@users.noreply.github.com>
2016-10-12 00:00:12 -0400

    bin/update_brew: remove universal-ctags

--- bin/update_brew.bash
+++ bin/update_brew.bash
@@ -4,7 +4,6 @@ brew update
 brew upgrade
 
 brew reinstall --HEAD --with-release neovim/neovim/neovim
-brew reinstall --HEAD universal-ctags/universal-ctags/universal-ctags
 
 brew cask update
 brew cleanup -s
