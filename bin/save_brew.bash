ccf9f6f0bd5a3eaf43f11919fac07c5c4e1fa821
Francisco Souza <fsouza@users.noreply.github.com>
2016-12-05 23:22:43 -0500

    bin/save_brew: fix indentation
    I have no idea what's going on.

--- bin/save_brew.bash
+++ bin/save_brew.bash
@@ -1,18 +1,18 @@
 #!/bin/bash -l
 
 function backup_line() {
-				formula_name="$1"
-				from_source=$(brew info $formula_name | grep "Built from source on.\+with: .\+")
-				if [ -n "${from_source}" ]; then
-								flags=$(echo $from_source | sed -e "s/^Built from source on .* with: //")
-								formula_name="$formula_name $flags"
-				fi
-				echo $formula_name
+	formula_name="$1"
+	from_source=$(brew info $formula_name | grep "Built from source on.\+with: .\+")
+	if [ -n "${from_source}" ]; then
+		flags=$(echo $from_source | sed -e "s/^Built from source on .* with: //")
+		formula_name="$formula_name $flags"
+	fi
+	echo $formula_name
 }
 
 >"$1"
 for formula in $(brew leaves); do
-				backup_line "${formula}" >> "$1"
+	backup_line "${formula}" >> "$1"
 done
 
 brew cask list > "${1}-cask"
