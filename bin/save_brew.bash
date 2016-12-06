3b5fd4af082a99a80a793d13b8a7270ceb0a8e6b
Francisco Souza <fsouza@users.noreply.github.com>
2016-12-05 23:16:15 -0500

    bin/save_brew: save flags used to build formulaes from source

--- bin/save_brew.bash
+++ bin/save_brew.bash
@@ -1,4 +1,18 @@
-#!/bin/bash -el
+#!/bin/bash -l
 
-brew leaves > "$1"
+function backup_line() {
+				formula_name="$1"
+				from_source=$(brew info $formula_name | grep "Built from source on.\+with: .\+")
+				if [ -n "${from_source}" ]; then
+								flags=$(echo $from_source | sed -e "s/^Built from source on .* with: //")
+								formula_name="$formula_name $flags"
+				fi
+				echo $formula_name
+}
+
+>"$1"
+for formula in $(brew leaves); do
+				backup_line "${formula}" >> "$1"
+done
+
 brew cask list > "${1}-cask"
