42cb4bd20bd456d7373551dc22b13031533c98eb
Francisco Souza <fsouza@users.noreply.github.com>
2018-12-24 20:57:31 -0500

    bin/save_brew: account for --HEAD
    I blame neovim.

--- bin/save_brew.bash
+++ bin/save_brew.bash
@@ -3,11 +3,18 @@
 function backup_line() {
 	formula_name="$1"
 	from_source=$(brew info $formula_name | grep "Built from source on.\+with: .\+")
+	from_head=$(brew info $formula_name | grep -B1 "Built from source on" | grep /HEAD-)
+
+	flags=""
 	if [ -n "${from_source}" ]; then
 		flags=$(echo $from_source | sed -e "s/^Built from source on .* with: //")
-		formula_name="$formula_name $flags"
 	fi
-	echo $formula_name
+
+	if [ -n "${from_head}" ]; then
+		flags="--HEAD $flags"
+	fi
+
+	echo "$formula_name $flags"
 }
 
 >"$1"
