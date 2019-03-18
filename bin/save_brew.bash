514d33dc197c7487ee3b86d0b4ad1137683d60ce
Francisco Souza <fsouza@users.noreply.github.com>
2019-03-17 21:02:28 -0400

    bin/save_brew: use brew info --installed --json as the source

--- bin/save_brew.bash
+++ bin/save_brew.bash
@@ -1,30 +1,8 @@
-#!/bin/bash -l
+#!/bin/bash -el
 
-function backup_line() {
-	formula_name="$1"
-	from_source=$(brew info $formula_name | grep "Built from source on.\+with: .\+")
-	from_head=$(brew info $formula_name | grep -B1 "Built from source on" | grep /HEAD-)
-
-	flags=""
-	if [ -n "${from_source}" ]; then
-		flags=$(echo $from_source | sed -e "s/^Built from source on .* with: //")
-	fi
-
-	if [ -n "${from_head}" ]; then
-		flags="--HEAD $flags"
-	fi
-
-	if [ -n "${flags}" ]; then
-		formula_name="$formula_name $flags"
-	fi
-
-	echo $formula_name
-}
-
->"$1"
-for formula in $(brew leaves); do
-	backup_line "${formula}" >> "$1"
-done
+brew info --installed --json | \
+	 jq -r '.[] | if .installed[].installed_on_request then . else empty end | (.name + " " + (.installed[].used_options | join (" ")) + " " + (if (.installed[].version | test("^HEAD-")) then "--HEAD" else "" end) + " " + (if (.installed[].poured_from_bottle | not) then "-s" else "" end)) | rtrimstr(" ")' \
+	 > "$1"
 
 brew tap > "${1}-tap"
 if [[ ${OS_NAME} == "Darwin" ]]; then
