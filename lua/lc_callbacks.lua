7e4c064d838aa76bdc9310533dd9e9e55591468d
francisco souza <fsouza@users.noreply.github.com>
2020-06-11 19:21:19 -0400

    Delete some dead code

--- lua/lc_callbacks.lua
+++ lua/lc_callbacks.lua
@@ -8,7 +8,6 @@ M['textDocument/hover'] = function(_, method, result)
     local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
     markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
     if vim.tbl_isempty(markdown_lines) then
-      -- return { 'No information available' }
       return
     end
     local bufnr, winnr = vim.lsp.util.fancy_floating_markdown(markdown_lines, {
