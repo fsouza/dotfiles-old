322141b8493a71d5b8cd981cffa17e9a92bdfc40
francisco souza <fsouza@users.noreply.github.com>
2020-06-11 22:18:39 -0400

    Fix code formatting

--- lua/lc.lua
+++ lua/lc.lua
@@ -127,20 +127,20 @@ function M.formatting_sync(options, timeout_ms)
 end
 
 function M.show_line_diagnostics()
-    local prefix = "- "
-    local indent = "  "
-    local lines = {"Diagnostics:", ""}
-    local line_diagnostics = vim.lsp.util.get_line_diagnostics()
-    if vim.tbl_isempty(line_diagnostics) then return end
+  local prefix = "- "
+  local indent = "  "
+  local lines = {"Diagnostics:", ""}
+  local line_diagnostics = vim.lsp.util.get_line_diagnostics()
+  if vim.tbl_isempty(line_diagnostics) then return end
 
-    for _, diagnostic in pairs(line_diagnostics) do
-      local message_lines = vim.split(diagnostic.message, '\n', true)
-      table.insert(lines, prefix..message_lines[1])
-      for j = 2, #message_lines do
-        table.insert(lines, indent..message_lines[j])
-      end
+  for _, diagnostic in pairs(line_diagnostics) do
+    local message_lines = vim.split(diagnostic.message, '\n', true)
+    table.insert(lines, prefix..message_lines[1])
+    for j = 2, #message_lines do
+      table.insert(lines, indent..message_lines[j])
     end
-    return vim.lsp.util.open_floating_preview(lines, 'plaintext')
+  end
+  return vim.lsp.util.open_floating_preview(lines, 'plaintext')
 end
 
 return M
