acabb3e5d950f0b58dc3f6cbfd130bb4cb88c1f9
francisco souza <fsouza@users.noreply.github.com>
2020-06-13 22:33:48 -0400

    lc: convert paths to relative before feeding them to fzf

--- lua/lc_callbacks.lua
+++ lua/lc_callbacks.lua
@@ -2,8 +2,13 @@ local M = {}
 
 local function format_items_for_fzf(items)
   local lines = {}
+  local prefix = vim.fn.getcwd() .. '/'
   for _, item in pairs(items) do
-    table.insert(lines, string.format('%s:%d:%d:%s', item.filename, item.lnum, item.col, item.text))
+    local filename = item.filename
+    if vim.startswith(filename, prefix) then
+      filename = string.sub(filename, string.len(prefix) + 1)
+    end
+    table.insert(lines, string.format('%s:%d:%d:%s', filename, item.lnum, item.col, item.text))
   end
   return lines
 end
