7abc53bd211541788179e3181d7becd318c7df3c
francisco souza <fsouza@users.noreply.github.com>
2020-06-15 23:05:55 -0400

    Move deoplete customizations to lua

--- after/plugin/deoplete.vim
+++
@@ -1,8 +0,0 @@
-call deoplete#custom#option({
-			\ 'auto_complete': v:false,
-			\ 'auto_refresh_delay': 0,
-			\ })
-
-call deoplete#custom#source('_', 'matchers', ['matcher_full_fuzzy'])
-
-imap <expr> <silent> <c-x><c-o> fsouza#complete#Complete()
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,7 @@
+-- his is fine because there aren't a lot of plugins. If the number of plugins
+-- starts to grow, I may need to look into some alternative (or maybe not, as
+-- neovim may support plugin handling in lua in the future.
+
 local M = {}
 
 local helpers = require('nvim_helpers')
@@ -15,8 +19,28 @@ local setup_fzf_mappings = function()
   })
 end
 
+local setup_deoplete = function()
+  vim.api.nvim_call_function('deoplete#custom#option', {
+    {
+      auto_complete = false;
+      auto_refresh_delay = 0;
+    };
+  })
+  vim.api.nvim_call_function('deoplete#custom#source', {
+    '_';
+    'matchers';
+    { 'matcher_full_fuzzy' };
+  })
+  helpers.create_mappings({
+    i = {
+      ['<c-x><c-o>'] = { 'fsouza#complete#Complete()'; { expr = true; silent = true } };
+    };
+  })
+end
+
 function M.setup_async()
   vim.schedule(setup_fzf_mappings)
+  vim.schedule(setup_deoplete)
 end
 
 return M
