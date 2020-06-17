230c04f39963f533ba8fc7cbf49544a22d839f2b
francisco souza <fsouza@users.noreply.github.com>
2020-06-17 18:16:50 -0400

    Add some docs

--- lua/global.lua
+++ lua/global.lua
@@ -1,6 +1,11 @@
+-- This module is available to be used from vim as v:lua.f.<module>.<function>.
+--
+-- Members are loaded lazily.
 local M = {}
 
 local meta = {}
-function meta:__index(key) return require('global/' .. key) end
+function meta:__index(key)
+  return require('global/' .. key)
+end
 
 return setmetatable(M, meta)
