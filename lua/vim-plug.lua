611bfd3508e8b8f6cd65a3e57a637ebc8c2c2b63
francisco souza <fsouza@users.noreply.github.com>
2020-06-16 20:54:30 -0400

    Reorganize the code for vim-plug
    Make it easier to read.

--- lua/vim-plug.lua
+++ lua/vim-plug.lua
@@ -2,37 +2,38 @@ local get_pkgs = function()
   local brew_prefix = vim.loop.os_getenv('HOMEBREW_PREFIX') or '/usr/local'
 
   return {
-    { 'editorconfig/editorconfig-vim' };
-    { 'fszymanski/fzf-quickfix'; { on = 'FzfQuickfix' }};
-    { 'godlygeek/tabular' };
-    { brew_prefix .. '/opt/fzf' };
-    { 'junegunn/fzf.vim' };
-    { 'justinmk/vim-dirvish' };
-    { 'justinmk/vim-sneak' };
-    { 'leafgarland/typescript-vim'; { ['for'] = 'typescript' } };
-    { 'michaeljsmith/vim-indent-object' };
-    { 'neovim/nvim-lsp'; { as = 'nvim-lsp' } };
-    { 'ncm2/float-preview.nvim' };
-    { 'ocaml/vim-ocaml'; { ['for'] = 'ocaml' }};
-    { 'Shougo/deoplete.nvim'; { ['do'] = ':UpdateRemotePlugins' }};
-    { 'Shougo/deoplete-lsp' };
-    { 'SirVer/ultisnips' };
-    { 'honza/vim-snippets' };
-    { 'tpope/vim-commentary' };
-    { 'tpope/vim-fugitive' };
-    { 'tpope/vim-repeat' };
-    { 'tpope/vim-surround' };
-    { 'Vimjas/vim-python-pep8-indent'; { ['for'] = 'python' }};
+    { repo = 'editorconfig/editorconfig-vim' };
+    { repo = 'fszymanski/fzf-quickfix'; opts = { on = 'FzfQuickfix' }};
+    { repo = 'godlygeek/tabular' };
+    { repo = brew_prefix .. '/opt/fzf' };
+    { repo = 'junegunn/fzf.vim' };
+    { repo = 'justinmk/vim-dirvish' };
+    { repo = 'justinmk/vim-sneak' };
+    { repo = 'leafgarland/typescript-vim'; opts = { ['for'] = 'typescript' } };
+    { repo = 'michaeljsmith/vim-indent-object' };
+    { repo = 'neovim/nvim-lsp'; opts = { ['as'] = 'nvim-lsp' }; eager = true };
+    { repo = 'ncm2/float-preview.nvim' };
+    { repo = 'ocaml/vim-ocaml'; opts = { ['for'] = 'ocaml' }};
+    { repo = 'Shougo/deoplete.nvim'; opts = { ['do'] = ':UpdateRemotePlugins' }};
+    { repo = 'Shougo/deoplete-lsp' };
+    { repo = 'SirVer/ultisnips' };
+    { repo = 'honza/vim-snippets' };
+    { repo = 'tpope/vim-commentary' };
+    { repo = 'tpope/vim-fugitive' };
+    { repo = 'tpope/vim-repeat' };
+    { repo = 'tpope/vim-surround' };
+    { repo = 'Vimjas/vim-python-pep8-indent'; opts = { ['for'] = 'python' }};
   }
 end
 
 return function(path)
+  local empty_dict = vim.empty_dict()
   vim.fn['plug#begin'](path)
   for _, args in ipairs(get_pkgs()) do
-    vim.api.nvim_call_function('plug#', args)
+    vim.fn['plug#'](args.repo, args.opts or empty_dict)
 
     if args[2] and args[2].as then
-      vim.api.nvim_call_function('plug#load', { args[2].as })
+      vim.fn['plug#load'](args.opts.as)
     end
   end
   vim.fn['plug#end']()
