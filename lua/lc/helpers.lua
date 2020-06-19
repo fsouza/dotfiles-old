5981defb30793ac1854c552acf26ce07b1c1407c
francisco souza <fsouza@users.noreply.github.com>
2020-06-19 00:23:37 -0400

    Set UseDefaultDatabase to true on pyls_ms

--- lua/lc/helpers.lua
+++ lua/lc/helpers.lua
@@ -4,7 +4,7 @@ local cmd = require('lib/cmd')
 
 function M.interpreter_properties_from_virtualenv(virtual_env)
   local int_path = virtual_env .. 'bin/python'
-  local props = {InterpreterPath = virtual_env .. 'bin/python'}
+  local props = {InterpreterPath = virtual_env .. 'bin/python'; UseDefaultDatabase = true}
   local cb = function(r)
     if r.exit_status ~= 0 then
       print(string.format('failed to detect python version in the virtualenv $%s: %s', virtual_env,
