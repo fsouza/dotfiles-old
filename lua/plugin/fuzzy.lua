6918b7fa8bb2742c086980a8d1efb6bec4bb286c
francisco souza <fsouza@users.noreply.github.com>
2020-06-19 22:12:30 -0400

    fuzzy: pass --hidden to rg (and ignore stuff accordingly)

--- lua/plugin/fuzzy.lua
+++ lua/plugin/fuzzy.lua
@@ -3,8 +3,9 @@ local M = {}
 function M.rg()
   local input = vim.fn.input([[rg \ ]])
   if input ~= '' then
-    local cmd = 'rg --column --line-number --no-heading --color=always --smart-case -- ' ..
-                  vim.fn.shellescape(input)
+    local cmd =
+      [[rg --column --line-number --hidden --no-heading --color=always --smart-case --glob '!.git' --glob '!.hg' -- ]] ..
+        vim.fn.shellescape(input)
     vim.fn['fzf#vim#grep'](cmd, true)
   end
 end
