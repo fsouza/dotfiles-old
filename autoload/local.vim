2f029662bc4bb1e5c224f3d422c84ab0f59b6e51
francisco souza <fsouza@users.noreply.github.com>
2020-06-19 22:41:29 -0400

    Add --multi to Lsp_fzf
    When multiple items are selected, the default action is to build a
    quickfix list.

--- autoload/local.vim
+++ autoload/local.vim
@@ -2,6 +2,6 @@ function local#Lsp_fzf(items)
 	call fzf#run(fzf#wrap(fzf#vim#with_preview({
 				\ 'source': a:items,
 				\ 'sink*': {lines -> v:lua.f.fzf.handle_lsp_line(lines)},
-				\ 'options': '--expect=ctrl-t,ctrl-x,ctrl-v',
+				\ 'options': '--expect=ctrl-t,ctrl-x,ctrl-v --multi',
 				\ })))
 endfunction
--- lua/global/fzf.lua
+++ lua/global/fzf.lua
@@ -1,23 +1,46 @@
 local M = {}
 
-local fzf_actions = {['ctrl-t'] = 'tabedit'; ['ctrl-x'] = 'split'; ['ctrl-v'] = 'vsplit'}
+local api = vim.api
+local lsp = vim.lsp
 local nvim_command = vim.api.nvim_command
 local nvim_input = vim.api.nvim_input
+
+local fzf_actions = {['ctrl-t'] = 'tabedit'; ['ctrl-x'] = 'split'; ['ctrl-v'] = 'vsplit'}
+
+local lines_to_qf = function(lines)
+  local items = {}
+  for _, line in ipairs(lines) do
+    local _, _, filename, lnum, col, text = string.find(line, '([^:]+):(%d+):(%d+):(.*)')
+    if filename then
+      table.insert(items, {filename = filename; lnum = lnum; col = col; text = text})
+    end
+  end
+  return items
+end
 
 function M.handle_lsp_line(lines)
   if #lines < 2 then
     return
   end
 
-  local action = fzf_actions[lines[1]] or 'edit'
-  local _, _, filename, lnum, cnum = string.find(lines[2], '([^:]+):(%d+):(%d+)')
-  if not filename then
+  local first_line = table.remove(lines, 1)
+  local action = fzf_actions[first_line] or 'edit'
+  local qf_list = lines_to_qf(lines)
+  if #qf_list < 1 then
     return
   end
 
-  nvim_command(string.format('%s %s', action, filename))
-  vim.fn.cursor(lnum, cnum)
-  nvim_input('zz')
+  if #qf_list == 1 then
+    local item = qf_list[1]
+    nvim_command(string.format('%s %s', action, item.filename))
+    vim.fn.cursor(item.lnum, item.col)
+    nvim_input('zz')
+  else
+    lsp.util.set_qflist(qf_list)
+    api.nvim_command('copen')
+    api.nvim_command('wincmd p')
+    api.nvim_command('cc')
+  end
 end
 
 return M
