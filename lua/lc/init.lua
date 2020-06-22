495c245eacb6517a9ec1692025794380db81d45f
francisco souza <fsouza@users.noreply.github.com>
2020-06-22 14:17:17 -0400

    lc: only configure servers if they're available

--- lua/lc/init.lua
+++ lua/lc/init.lua
@@ -1,3 +1,4 @@
+local vfn = vim.fn
 local loop = vim.loop
 local lc_opts = require('lc/opts')
 
@@ -17,8 +18,7 @@ local python_interpreter_props = function(virtual_env)
     props.Version = r.stdout
   end
   require('lib/cmd').run(props.InterpreterPath, {
-    '-c';
-    'import sys; print("f{sys.version_info.major}.{sys.version_info.minor}"), end="")';
+    '-c'; 'import sys; print("f{sys.version_info.major}.{sys.version_info.minor}"), end="")';
   }, nil, cb)
   vim.wait(200, function()
     return props.Version ~= nil
@@ -39,74 +39,94 @@ local get_pyls_ms_options = function()
 end
 
 do
+  local if_executable = function(name, cb)
+    if vfn.executable(name) == 1 then
+      cb()
+    end
+  end
+
   local status, err = pcall(function()
     local lsp = require('nvim_lsp')
     local vim_node_ls = get_local_cmd('node-lsp')
 
-    lsp.bashls.setup(lc_opts.with_default_opts({
-      cmd = {vim_node_ls; 'bash-language-server'; 'start'};
-    }))
+    if_executable('npx', function()
+      lsp.bashls.setup(lc_opts.with_default_opts({
+        cmd = {vim_node_ls; 'bash-language-server'; 'start'};
+      }))
 
-    lsp.cssls.setup(
-      lc_opts.with_default_opts({cmd = {vim_node_ls; 'css-laguageserver'; '--stdio'}}))
+      lsp.cssls.setup(lc_opts.with_default_opts({
+        cmd = {vim_node_ls; 'css-laguageserver'; '--stdio'};
+      }))
 
-    lsp.gopls.setup(lc_opts.with_default_opts({
-      cmd = {get_local_cmd('go-lsp')};
-      init_options = {
-        deepCompletion = false;
-        staticcheck = true;
-        analyses = {unusedparams = true; ST1000 = false};
-      };
-      capabilities = {textDocument = {completion = {completionItem = {snippetSupport = false}}}};
-    }))
+      lsp.html
+        .setup(lc_opts.with_default_opts({cmd = {vim_node_ls; 'html-langserver'; '--stdio'}}))
 
-    lsp.html.setup(lc_opts.with_default_opts({cmd = {vim_node_ls; 'html-langserver'; '--stdio'}}))
+      lsp.jsonls.setup(lc_opts.with_default_opts({
+        cmd = {vim_node_ls; 'vscode-json-languageserver'; '--stdio'};
+      }))
 
-    lsp.jsonls.setup(lc_opts.with_default_opts({
-      cmd = {vim_node_ls; 'vscode-json-languageserver'; '--stdio'};
-    }))
+      lsp.tsserver.setup(lc_opts.with_default_opts(
+                           {cmd = {vim_node_ls; 'typescript-language-server'; '--stdio'}}))
 
-    lsp.ocamllsp.setup(lc_opts.with_default_opts({cmd = {get_local_cmd('ocaml-lsp')}}))
+      lsp.vimls.setup(lc_opts.with_default_opts({
+        cmd = {vim_node_ls; 'vim-language-server'; '--stdio'};
+      }))
 
-    lsp.pyls.setup(lc_opts.with_default_opts({
-      cmd = {'python'; '-m'; 'pyls'};
-      root_dir = function(fname)
-        local ancestor = lc_opts.project_root_pattern(fname)
-        if not ancestor then
-          return vim.fn.getcwd()
-        end
-        return ancestor
-      end;
-      settings = {
-        pyls = {
-          enable = true;
-          plugins = {
-            jedi_completion = {enabled = false};
-            jedi_hover = {enabled = false};
-            jedi_rename = {enabled = false};
-            jedi_references = {enabled = false};
-            jedi_signature_help = {enabled = false};
-            jedi_symbols = {enabled = false};
+      lsp.yamlls.setup(lc_opts.with_default_opts({
+        cmd = {vim_node_ls; 'yaml-language-server'; '--stdio'};
+      }))
+    end)
+
+    if_executable(get_local_cmd('gopls'), function()
+      lsp.gopls.setup(lc_opts.with_default_opts({
+        cmd = {get_local_cmd('go-lsp')};
+        init_options = {
+          deepCompletion = false;
+          staticcheck = true;
+          analyses = {unusedparams = true; ST1000 = false};
+        };
+        capabilities = {textDocument = {completion = {completionItem = {snippetSupport = false}}}};
+      }))
+    end)
+
+    if_executable('dune', function()
+      lsp.ocamllsp.setup(lc_opts.with_default_opts({cmd = {get_local_cmd('ocaml-lsp')}}))
+    end)
+
+    if_executable('pyls', function()
+      lsp.pyls.setup(lc_opts.with_default_opts({
+        cmd = {'python'; '-m'; 'pyls'};
+        root_dir = function(fname)
+          local ancestor = lc_opts.project_root_pattern(fname)
+          if not ancestor then
+            return vim.fn.getcwd()
+          end
+          return ancestor
+        end;
+        settings = {
+          pyls = {
+            enable = true;
+            plugins = {
+              jedi_completion = {enabled = false};
+              jedi_hover = {enabled = false};
+              jedi_rename = {enabled = false};
+              jedi_references = {enabled = false};
+              jedi_signature_help = {enabled = false};
+              jedi_symbols = {enabled = false};
+            };
           };
         };
-      };
-    }))
+      }))
+    end)
 
-    lsp.pyls_ms.setup(lc_opts.with_default_opts(get_pyls_ms_options()))
+    if_executable('dotnet', function()
+      lsp.pyls_ms.setup(lc_opts.with_default_opts(get_pyls_ms_options()))
+    end)
 
-    lsp.rust_analyzer.setup(lc_opts.with_default_opts({cmd = {get_local_cmd('rust-analyzer')}}))
-
-    lsp.tsserver.setup(lc_opts.with_default_opts({
-      cmd = {vim_node_ls; 'typescript-language-server'; '--stdio'};
-    }))
-
-    lsp.vimls.setup(lc_opts.with_default_opts({
-      cmd = {vim_node_ls; 'vim-language-server'; '--stdio'};
-    }))
-
-    lsp.yamlls.setup(lc_opts.with_default_opts({
-      cmd = {vim_node_ls; 'yaml-language-server'; '--stdio'};
-    }))
+    local ra = get_local_cmd('rust-analyzer')
+    if_executable('ra', function()
+      lsp.rust_analyzer.setup(lc_opts.with_default_opts({cmd = {ra}}))
+    end)
   end)
 
   if loop.os_getenv('NVIM_DEBUG') and not status then
