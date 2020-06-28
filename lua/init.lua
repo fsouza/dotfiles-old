8b474ef9d8e29d7135bbeda7db29ac3127d1bb8f
francisco souza <fsouza@users.noreply.github.com>
2020-06-27 22:45:00 -0400

    Update m' on long jumps

--- lua/init.lua
+++ lua/init.lua
@@ -116,7 +116,15 @@ local global_mappings = function()
   local maps = {
     n = {
       {lhs = '<leader>o'; rhs = helpers.cmd_map('only')};
-      {lhs = '<leader>O'; rhs = helpers.cmd_map('only') .. helpers.cmd_map('tabonly')};
+      {lhs = '<leader>O'; rhs = helpers.cmd_map('only') .. helpers.cmd_map('tabonly')}; {
+        lhs = 'j';
+        rhs = [[(v:count > 8 ? "m'" . v:count : '') . 'j']];
+        opts = {expr = true; noremap = true};
+      }; {
+        lhs = 'k';
+        rhs = [[(v:count > 8 ? "m'" . v:count : '') . 'k']];
+        opts = {expr = true; noremap = true};
+      };
     };
     i = {
       {lhs = '<c-f>'; rhs = '<right>'; opts = {noremap = true}};
