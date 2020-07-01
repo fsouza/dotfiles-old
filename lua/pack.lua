13350dd35a858d47b5e63d4e2850b9898b9abe56
francisco souza <fsouza@users.noreply.github.com>
2020-07-01 08:13:15 -0400

    pack: don't call syntax_and_filetype in a scheduled function
    Turns out doing that hurts more because neovim had to process
    nosyntax.vim (and that was quite slow).

--- lua/pack.lua
+++ lua/pack.lua
@@ -3,25 +3,23 @@ local M = {}
 local vcmd = vim.cmd
 local helpers = require('lib.nvim_helpers')
 
-local pkgs = function()
-  return {
-    'completion-nvim'; 'fzf.vim'; 'nvim-lsp'; 'tabular'; 'vim-commentary'; 'vim-dirvish';
-    'vim-fugitive'; 'vim-indent-object'; 'vim-repeat'; 'vim-sneak'; 'vim-surround';
-    'nvim-treesitter'; 'vista.vim'; 'completion-buffers'; 'vim-polyglot';
-  }
-end
+local pkgs = {
+  'completion-nvim'; 'fzf.vim'; 'nvim-lsp'; 'tabular'; 'vim-commentary'; 'vim-dirvish';
+  'vim-fugitive'; 'vim-indent-object'; 'vim-repeat'; 'vim-sneak'; 'vim-surround';
+  'nvim-treesitter'; 'vista.vim'; 'completion-buffers'; 'vim-polyglot';
+}
 
 local syntax_and_filetype = function()
   helpers.exec_cmds({'syntax enable'; 'filetype plugin indent on'})
 end
 
 function M.setup()
-  for _, pkg in ipairs(pkgs()) do
+  for _, pkg in ipairs(pkgs) do
     vcmd('packadd! ' .. pkg)
   end
 
+  syntax_and_filetype()
   vim.schedule(function()
-    syntax_and_filetype()
     vcmd([[doautocmd User PluginReady]])
   end)
 end
