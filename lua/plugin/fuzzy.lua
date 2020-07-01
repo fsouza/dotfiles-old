f32aba58dedbdba32699f0dd4ac02f6eeca70056
francisco souza <fsouza@users.noreply.github.com>
2020-06-30 20:59:18 -0400

    plugin/fuzzy: simplify
    turns out I needed to rtfm

--- lua/plugin/fuzzy.lua
+++ lua/plugin/fuzzy.lua
@@ -2,17 +2,13 @@ local M = {}
 
 local vfn = vim.fn
 
-local fzf_preview_bin = vfn.shellescape(vfn.stdpath('config') ..
-                                          '/pack/b/opt/fzf.vim/bin/preview.sh')
-
 function M.rg(input)
   input = input or vfn.input([[rg：]])
   if input ~= '' then
     local cmd =
       [[rg --column --line-number --hidden --no-heading --color=always --smart-case --glob '!.git' --glob '!.hg' -- ]] ..
         vfn.shellescape(input)
-    vfn['fzf#vim#grep'](cmd, true,
-                        {options = {'--preview'; string.format([[%s {}]], fzf_preview_bin)}})
+    vfn['fzf#vim#grep'](cmd, true, vfn['fzf#vim#with_preview']())
   end
 end
 
