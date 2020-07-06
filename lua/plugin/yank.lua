bdcd23af4243d3978c275069abd5888485937811
francisco souza <fsouza@users.noreply.github.com>
2020-07-06 13:21:50 -0400

    plugin/yank: enable on_visual

--- lua/plugin/yank.lua
+++ lua/plugin/yank.lua
@@ -1,12 +1,7 @@
 local M = {}
 
 function M.on_yank()
-  require('vim.highlight').on_yank({
-    higroup = 'HlYank';
-    timeout = 200;
-    on_visual = false;
-    on_macro = false;
-  })
+  require('vim.highlight').on_yank({higroup = 'HlYank'; timeout = 200; on_macro = false})
 end
 
 return M
