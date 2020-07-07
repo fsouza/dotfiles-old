088b1a509f1028ca24099b2484f38b9f07a911f9
francisco souza <fsouza@users.noreply.github.com>
2020-07-07 07:52:17 -0400

    lc/locations: bufload before parsing
    Still not working, will investigate later.

--- lua/lc/locations.lua
+++ lua/lc/locations.lua
@@ -21,6 +21,7 @@ local ts_range = function(loc)
   local bufnr = vim.uri_to_bufnr(loc.uri)
   vim.api.nvim_buf_set_option(bufnr, 'buflisted', true)
   vim.api.nvim_buf_set_option(bufnr, 'filetype', vim.bo.filetype)
+  vim.fn.bufload(vim.fn.expand(string.format('#%d:p', bufnr)))
 
   local start_pos = loc.range.start
   local end_pos = loc.range['end']
@@ -29,6 +30,7 @@ local ts_range = function(loc)
   local root = parser.tree:root()
   local node = root:named_descendant_for_range(start_pos.line, start_pos.character, end_pos.line,
                                                end_pos.character)
+
   local scope = ts_utils.containing_scope(node)
   if scope:type() == 'function_declaration' or scope:type() == 'method_declaration' then
     local sl, sc, el, ec = scope:range()
@@ -40,7 +42,7 @@ local ts_range = function(loc)
   return loc
 end
 
-local preview_location_callback = function(_, _, result)
+local peek_location_callback = function(_, _, result)
   if not result or vim.tbl_isempty(result) then
     return
   end
@@ -51,22 +53,22 @@ end
 
 function M.preview_definition()
   local params = lap_util.make_position_params()
-  lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
+  lsp.buf_request(0, 'textDocument/definition', params, peek_location_callback)
 end
 
 function M.preview_declaration()
   local params = lap_util.make_position_params()
-  lsp.buf_request(0, 'textDocument/declaration', params, preview_location_callback)
+  lsp.buf_request(0, 'textDocument/declaration', params, peek_location_callback)
 end
 
 function M.preview_implementation()
   local params = lap_util.make_position_params()
-  lsp.buf_request(0, 'textDocument/implementation', params, preview_location_callback)
+  lsp.buf_request(0, 'textDocument/implementation', params, peek_location_callback)
 end
 
 function M.preview_type_definition()
   local params = lap_util.make_position_params()
-  lsp.buf_request(0, 'textDocument/typeDefinition', params, preview_location_callback)
+  lsp.buf_request(0, 'textDocument/typeDefinition', params, peek_location_callback)
 end
 
 return M
