743e89d4ef66d33ada8df2d5b76f56d5d9cae4c2
francisco souza <fsouza@users.noreply.github.com>
2020-08-10 23:45:23 -0400

    bin/save_brew: save mas too

--- bin/save_brew.bash
+++ bin/save_brew.bash
@@ -1,10 +1,14 @@
 #!/bin/zsh -el
 
-basedir=$(cd $(dirname $0)/..; pwd -P)
+basedir=$(
+	cd $(dirname $0)/..
+	pwd -P
+)
 
-brew info --installed --json | jq -rf $basedir/extra/brew-info.jq > "$1"
+brew info --installed --json | jq -rf $basedir/extra/brew-info.jq >"$1"
 
-brew tap > "${1}-tap"
+brew tap >"${1}-tap"
 if [[ ${OS_NAME} == "Darwin" ]]; then
-	brew cask list > "${1}-cask"
+	brew cask list >"${1}-cask"
 fi
+mas list >"${1}-mas"
