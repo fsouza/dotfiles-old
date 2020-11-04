947f41a7123c842fe198ed5fa06333eb42d4eaa0
francisco souza <fsouza@users.noreply.github.com>
2020-11-03 19:01:21 -0500

    lua/packed: pass some config options to packer

--- lua/packed.lua
+++ lua/packed.lua
@@ -28,90 +28,108 @@ local add_sync_commands = function()
   vcmd([[command! -bar PackerSyncSync lua require('packed').sync('sync')]])
 end
 
-do
-  require('packer').startup(function(use)
-    use({'wbthomason/packer.nvim'; opt = true})
+local setup_packer = function(use)
+  use({'wbthomason/packer.nvim'; opt = true})
 
-    use({'godlygeek/tabular'; opt = true; cmd = {'Tabularize'}})
-    use({
-      'junegunn/fzf.vim';
-      opt = true;
-      cmd = {'FzfFiles'; 'FzfCommands'; 'FzfBuffers'; 'FzfLines'};
-    })
-    use({'justinmk/vim-dirvish'})
-    use({
-      'justinmk/vim-sneak';
-      opt = true;
-      keys = {
-        {'n'; 's'};
-        {'n'; 'S'};
-        {'o'; 'z'};
-        {'o'; 'Z'};
-        {'n'; ';'};
-        {'n'; ','};
-        {'x'; ';'};
-        {'x'; ','};
-        {'o'; ';'};
-        {'o'; ','};
-      };
-    });
-    use({'neovim/nvim-lspconfig'})
-    use({'nvim-lua/completion-nvim'; opt = true})
-    use({'sheerun/vim-polyglot'})
-    use({
-      'tpope/vim-commentary';
-      opt = true;
-      keys = {{'n'; 'gcc'}; {'x'; 'gc'}; {'o'; 'gc'}; {'n'; 'gc'}};
-    })
-    use({'tpope/vim-repeat'})
-    use({
-      'tpope/vim-surround';
-      opt = true;
-      keys = {
-        {'n'; 'gS'};
-        {'n'; 'ds'};
-        {'n'; 'cs'};
-        {'n'; 'cS'};
-        {'n'; 'ys'};
-        {'n'; 'yS'};
-        {'n'; 'yss'};
-        {'n'; 'ySs'};
-        {'x'; 'S'};
-        {'x'; 'gS'};
-      };
-    })
-    use({
-      'mattn/emmet-vim';
-      opt = true;
-      keys = {
-        {'i'; '<C-X>m'};
-        {'i'; '<C-X>A'};
-        {'i'; '<C-X>a'};
-        {'i'; '<C-X>k'};
-        {'i'; '<C-X>j'};
-        {'i'; '<C-X>/'};
-        {'i'; '<C-X>I'};
-        {'i'; '<C-X>i'};
-        {'i'; '<C-X>N'};
-        {'i'; '<C-X>n'};
-        {'i'; '<C-X>D'};
-        {'i'; '<C-X>d'};
-        {'i'; '<C-X>u'};
-        {'i'; '<C-X>;'};
-        {'i'; '<C-X>,'};
-      };
-      cmd = {'Emmet'; 'EmmetInstall'};
-    })
-    use({'rhysd/git-messenger.vim'; opt = true; cmd = {'GitMessenger'}; keys = {'<leader>gm'}})
-    use({'norcalli/nvim-colorizer.lua'})
-    use({'kana/vim-textobj-user'})
-    use({'thinca/vim-textobj-between'})
-    use({'nvim-treesitter/nvim-treesitter'})
-    use({'nvim-treesitter/nvim-treesitter-textobjects'})
-    use({'nvim-treesitter/playground'})
-    use({'romgrk/nvim-treesitter-context'})
-    use({'michaeljsmith/vim-indent-object'})
-  end)
+  use({'godlygeek/tabular'; opt = true; cmd = {'Tabularize'}})
+  use({'junegunn/fzf.vim'; opt = true; cmd = {'FzfFiles'; 'FzfCommands'; 'FzfBuffers'; 'FzfLines'}})
+  use({'justinmk/vim-dirvish'})
+  use({
+    'justinmk/vim-sneak';
+    opt = true;
+    keys = {
+      {'n'; 's'};
+      {'n'; 'S'};
+      {'o'; 'z'};
+      {'o'; 'Z'};
+      {'n'; ';'};
+      {'n'; ','};
+      {'x'; ';'};
+      {'x'; ','};
+      {'o'; ';'};
+      {'o'; ','};
+    };
+  });
+  use({'neovim/nvim-lspconfig'})
+  use({'nvim-lua/completion-nvim'; opt = true})
+  use({'sheerun/vim-polyglot'})
+  use({
+    'tpope/vim-commentary';
+    opt = true;
+    keys = {{'n'; 'gcc'}; {'x'; 'gc'}; {'o'; 'gc'}; {'n'; 'gc'}};
+  })
+  use({'tpope/vim-repeat'})
+  use({
+    'tpope/vim-surround';
+    opt = true;
+    keys = {
+      {'n'; 'gS'};
+      {'n'; 'ds'};
+      {'n'; 'cs'};
+      {'n'; 'cS'};
+      {'n'; 'ys'};
+      {'n'; 'yS'};
+      {'n'; 'yss'};
+      {'n'; 'ySs'};
+      {'x'; 'S'};
+      {'x'; 'gS'};
+    };
+  })
+  use({
+    'mattn/emmet-vim';
+    opt = true;
+    keys = {
+      {'i'; '<C-X>m'};
+      {'i'; '<C-X>A'};
+      {'i'; '<C-X>a'};
+      {'i'; '<C-X>k'};
+      {'i'; '<C-X>j'};
+      {'i'; '<C-X>/'};
+      {'i'; '<C-X>I'};
+      {'i'; '<C-X>i'};
+      {'i'; '<C-X>N'};
+      {'i'; '<C-X>n'};
+      {'i'; '<C-X>D'};
+      {'i'; '<C-X>d'};
+      {'i'; '<C-X>u'};
+      {'i'; '<C-X>;'};
+      {'i'; '<C-X>,'};
+    };
+    cmd = {'Emmet'; 'EmmetInstall'};
+  })
+  use({'rhysd/git-messenger.vim'; opt = true; cmd = {'GitMessenger'}; keys = {'<leader>gm'}})
+  use({'norcalli/nvim-colorizer.lua'})
+  use({'kana/vim-textobj-user'})
+  use({
+    'thinca/vim-textobj-between';
+    opt = true;
+    keys = {{'x'; 'if'}; {'x'; 'af'}; {'o'; 'if'}; {'o'; 'af'}};
+  })
+  use({'nvim-treesitter/nvim-treesitter'})
+  use({'nvim-treesitter/nvim-treesitter-textobjects'})
+  use({'nvim-treesitter/playground'; opt = true; cmd = {'TSPlaygroundToggle'}})
+  use({'romgrk/nvim-treesitter-context'; opt = true; cmd = {'TSContextEnable'}})
+  use({
+    'michaeljsmith/vim-indent-object';
+    opt = true;
+    keys = {
+      {'x'; 'ii'};
+      {'x'; 'ai'};
+      {'x'; 'iI'};
+      {'x'; 'aI'};
+      {'o'; 'ii'};
+      {'o'; 'ai'};
+      {'o'; 'iI'};
+      {'o'; 'aI'};
+    };
+  })
+end
+
+do
+  require('packer').startup({
+    setup_packer;
+    config = {compile_on_sync = true; disable_commands = true};
+  })
   add_sync_commands()
   vim.schedule(function()
     vcmd([[doautocmd User PluginReady]])
