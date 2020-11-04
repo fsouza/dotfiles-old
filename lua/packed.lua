79a55d3ff83ce7e5e92b09a1bc1a90cab25eecb1
francisco souza <fsouza@users.noreply.github.com>
2020-11-03 21:08:18 -0500

    lua: remove the sync stuff for packer

--- lua/packed.lua
+++ lua/packed.lua
@@ -101,47 +101,6 @@ local deps = {
   };
 }
 
-local get_short_name = function(path)
-  local parts = vim.split(path, '/', true)
-  return parts[#parts]
-end
-
-local get_all_plugins = function()
-  local result = {}
-  local add_to_result = function(path)
-    local short_name = get_short_name(path)
-    result[short_name] = true
-  end
-  local plugin_utils = require('packer.plugin_utils')
-  local opt_plugins, start_plugins = plugin_utils.list_installed_plugins()
-  for k in pairs(opt_plugins) do
-    add_to_result(k)
-  end
-  for k in pairs(start_plugins) do
-    add_to_result(k)
-  end
-  return result
-end
-
-function M.install_and_wait(timeout_ms)
-  local min_ms = 60000
-  timeout_ms = timeout_ms or (2 * min_ms)
-  require('packer').install()
-  local status, _ = vim.wait(timeout_ms, function()
-    local plugins = get_all_plugins()
-    for _, dep in ipairs(deps) do
-      local dep_short_name = get_short_name(dep[1])
-      if not plugins[dep_short_name] then
-        return false
-      end
-    end
-    return true
-  end, 100)
-  if not status then
-    error(string.format('PackerInstall timed out after %dms', timeout_ms))
-  end
-end
-
 function M.reload()
   package.loaded['packed'] = nil
   require('packed').setup(true)
@@ -156,14 +115,9 @@ local setup_auto_commands = function()
   })
 end
 
-local setup_sync_commands = function()
-  vcmd([[command! -bar PackerInstallSync lua require('packed').install_and_wait()]])
-end
-
 function M.setup(reloading)
   require('packer').startup({deps; config = {compile_on_sync = true}})
   setup_auto_commands()
-  setup_sync_commands()
   if not reloading then
     vim.schedule(function()
       vcmd([[doautocmd User PluginReady]])
