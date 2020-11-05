c3ba13a75287ce3928b645d8d45b39416d582299
francisco souza <fsouza@users.noreply.github.com>
2020-11-05 14:06:37 -0500

    plugin/popup: exit early with the gitmessenger popup

--- lua/plugin/popup.lua
+++ lua/plugin/popup.lua
@@ -6,9 +6,11 @@ function M.set_theme_to_gitmessenger_popup()
   local bufinfo = vfn.getbufinfo()
   for _, buffer in ipairs(bufinfo) do
     if buffer.variables.current_syntax == 'gitmessengerpopup' then
-      for _, win_id in ipairs(buffer.windows) do
+      local win_id = next(buffer.windows)
+      if win_id then
         require('color').set_popup_winid(win_id)
       end
+      return
     end
   end
 end
