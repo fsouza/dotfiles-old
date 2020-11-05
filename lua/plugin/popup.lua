580964a4957ec0527dc88584b27077677b9f7a5a
francisco souza <fsouza@users.noreply.github.com>
2020-11-05 14:12:46 -0500

    plugin/popup: fix interaction with next()
    Opsss

--- lua/plugin/popup.lua
+++ lua/plugin/popup.lua
@@ -6,7 +6,7 @@ function M.set_theme_to_gitmessenger_popup()
   local bufinfo = vfn.getbufinfo()
   for _, buffer in ipairs(bufinfo) do
     if buffer.variables.current_syntax == 'gitmessengerpopup' then
-      local win_id = next(buffer.windows)
+      local _, win_id = next(buffer.windows)
       if win_id then
         require('color').set_popup_winid(win_id)
       end
