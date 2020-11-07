aa9b2b9c82f7ab6f63d986295b2e75fbe82eb95a
francisco souza <fsouza@users.noreply.github.com>
2020-11-07 18:20:59 -0500

    color: add function for setting the default theme
    Noticed that I can't override the default ns while reporting an issue to
    nvim lol

--- lua/color.lua
+++ lua/color.lua
@@ -3,16 +3,23 @@ local themes = require('themes')
 
 local M = {}
 
+local _default_theme
+
 local _themes = {}
 
 function M.set_popup_winid(winid)
   _themes[winid] = themes.popup
 end
 
+function M.set_default_theme(theme_ns)
+  _default_theme = theme_ns
+end
+
 function M.setup()
   vim.o.background = 'light'
+  _default_theme = themes.none
   local cb = function(_, winid)
-    local theme = _themes[winid] or themes.none
+    local theme = _themes[winid] or _default_theme
     api.nvim_set_hl_ns(theme)
   end
   api.nvim_set_decoration_provider(themes.none, {on_win = cb; on_line = cb})
