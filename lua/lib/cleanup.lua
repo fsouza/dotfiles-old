89a1506603de99c31e9835e13d67d3c99effccd7
francisco souza <fsouza@users.noreply.github.com>
2020-11-12 15:41:24 -0500

    lib/cleanup: fix invocation
    OMG lol

--- lua/lib/cleanup.lua
+++ lua/lib/cleanup.lua
@@ -24,7 +24,7 @@ end
 
 function M.setup()
   helpers.augroup('lua_lib_cleanup', {
-    {events = {'VimLeavePre'}; targets = {'*'}; command = [[lua require('lib.cleanup')]]};
+    {events = {'VimLeavePre'}; targets = {'*'}; command = [[lua require('lib.cleanup').cleanup()]]};
   })
 end
 
