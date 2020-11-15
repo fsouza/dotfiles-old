bd32f4efcb52fa7fac7cc342bba056fba45acbe1
francisco souza <fsouza@users.noreply.github.com>
2020-11-15 18:22:09 -0500

    themes: remove print and comment

--- lua/themes.lua
+++ lua/themes.lua
@@ -2,8 +2,6 @@ local M = {}
 
 local meta = {}
 function meta:__index(key)
-  print(key)
-  -- avoid dynamic requires: not good with tl.
   if key == 'popup' then
     M[key] = require('themes.popup')()
   else
