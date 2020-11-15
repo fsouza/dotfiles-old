382b19e4799d8280798168b32b49808526c8bc01
francisco souza <fsouza@users.noreply.github.com>
2020-11-15 02:11:57 -0500

    color: gc _themes on callback
    Because it grows over time.

--- lua/color.lua
+++ lua/color.lua
@@ -15,12 +15,21 @@ function M.set_default_theme(theme_ns)
   _default_theme = theme_ns
 end
 
+local gc = function()
+  for winid in pairs(_themes) do
+    if not api.nvim_win_is_valid(winid) then
+      _themes[winid] = nil
+    end
+  end
+end
+
 function M.setup()
   vim.o.background = 'light'
   _default_theme = themes.none
   local cb = function(_, winid)
     local theme = _themes[winid] or _default_theme
     api.nvim_set_hl_ns(theme)
+    vim.schedule(gc)
   end
   api.nvim_set_decoration_provider(themes.none, {on_win = cb; on_line = cb})
 end
