d9c08628bb26f413eab69924f1e9db5cfa458f41
francisco souza <fsouza@users.noreply.github.com>
2020-11-15 02:14:51 -0500

    lc/callbacks: fix popup_callback
    lmao

--- lua/lc/callbacks.lua
+++ lua/lc/callbacks.lua
@@ -14,8 +14,9 @@ end
 
 local popup_callback = function(err, method, result)
   vim.lsp.handlers[method](err, method, result)
-  for bufnr, winid in ipairs(api.nvim_list_wins()) do
+  for _, winid in ipairs(api.nvim_list_wins()) do
     if pcall(api.nvim_win_get_var, winid, method) then
+      local bufnr = api.nvim_win_get_buf(winid)
       require('color').set_popup_bufnr(bufnr)
     end
   end
