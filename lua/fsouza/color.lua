da88c040350b7f4bb44690d3ffee9cc34b96c35f
francisco souza <fsouza@users.noreply.github.com>
2020-12-26 15:31:22 -0500

    color: move gc to a timer
    Alternatively, I could've move that to on_end, but a timer makes more
    sense.

--- lua/fsouza/color.lua
+++ lua/fsouza/color.lua
@@ -9,6 +9,10 @@ local _themes = {}
 
 local _popup_cb
 
+local gc_interval_ms = 5000
+
+local timer
+
 -- Should we support more than one cb?
 function M.add_popup_cb(cb)
   _popup_cb = cb
@@ -45,6 +49,19 @@ local function find_theme(curr_winid)
   return _default_theme
 end
 
+local function start_gc_timer()
+  if not timer then
+    timer = vim.loop.new_timer()
+  end
+  timer:start(gc_interval_ms, gc_interval_ms, vim.schedule_wrap(gc))
+end
+
+local function stop_gc_timer()
+  if timer then
+    timer:close()
+  end
+end
+
 function M.enable()
   vim.o.background = 'light'
   _default_theme = themes.none
@@ -54,15 +71,16 @@ function M.enable()
       theme = find_theme(winid)
     end
     api.nvim_set_hl_ns(theme)
-    vim.schedule(gc)
   end
   local ns = api.nvim_create_namespace('fsouza__color')
   api.nvim_set_decoration_provider(ns, {on_win = cb; on_line = cb})
+  start_gc_timer()
 end
 
 function M.disable()
   _default_theme = 0
   _themes = {}
+  stop_gc_timer()
 end
 
 return M
