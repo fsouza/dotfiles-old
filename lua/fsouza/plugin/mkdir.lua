0f77820ded61785071cc26a7fbfca98d6b16a728
francisco souza <fsouza@users.noreply.github.com>
2020-12-28 11:12:22 -0500

    plugin/mkdir: make it smarter

--- lua/fsouza/plugin/mkdir.lua
+++ lua/fsouza/plugin/mkdir.lua
@@ -4,19 +4,30 @@ local helpers = require('fsouza.lib.nvim_helpers')
 
 local M = {}
 
-function M.run()
-  local bufnr = vfn.expand('<abuf>')
+function M.run(bufnr)
   local bufname = api.nvim_buf_get_name(bufnr)
   local dir = vfn.fnamemodify(bufname, ':h')
   vfn.mkdir(dir, 'p')
 end
 
+function M.register_for_buffer()
+  local bufnr = vfn.expand('<abuf>')
+  helpers.augroup('fsouza__mkdir_' .. bufnr, {
+    {
+      events = {'BufWritePre'};
+      targets = {string.format('<buffer=%d>', bufnr)};
+      modifiers = {'++once'};
+      command = string.format([[lua require('fsouza.plugin.mkdir').run(%d)]], bufnr);
+    };
+  })
+end
+
 function M.setup()
   helpers.augroup('fsouza__mkdir', {
     {
-      events = {'BufWritePre'};
+      events = {'BufNew'};
       targets = {'*'};
-      command = [[lua require('fsouza.plugin.mkdir').run()]];
+      command = [[lua require('fsouza.plugin.mkdir').register_for_buffer()]];
     };
   })
 end
